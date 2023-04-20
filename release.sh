32001287f836be1ab994fd3bcc8cd154b92b77b8
Sudhi S <sudhis@live.com>
2023-04-20 13:27:41 +0530

    Stable implementation

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-# git add .
-# git commit -m "Stable implementation" 
-# git push -u origin main 
+git add .
+git commit -m "Stable implementation" 
+git push -u origin main 
 flutter build web
 firebase deploy
