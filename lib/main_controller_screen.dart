c1aa55226233cd76c517a5974d324721c403ae56
Sudhi S <sudhis@live.com>
2023-04-20 13:49:38 +0530

    Stable implementation

--- lib/main_controller_screen.dart
+++ lib/main_controller_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:formstack/formstack.dart';
-import 'package:formstack_example_one/form.dart';
 
 class MainControllerScreen extends StatefulWidget {
   const MainControllerScreen({super.key});
@@ -10,22 +9,9 @@ class MainControllerScreen extends StatefulWidget {
 }
 
 class _MainControllerScreenState extends State<MainControllerScreen> {
-  var mainKey = UniqueKey();
-
-  @override
-  void initState() {
-    super.initState();
-    FormConfig.refreshState(
-      () {
-        mainKey = UniqueKey();
-        setState(() {});
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     //You can call by name FormStack.api().render(name: "auth")
-    return Container(key: mainKey, child: FormStack.api().render());
+    return Container(child: FormStack.api().render());
   }
 }
